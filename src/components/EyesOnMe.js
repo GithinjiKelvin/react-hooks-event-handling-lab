163cbd212cb7596061b3a50012b799b180af5589
GithinjiKelvin <githinjikelvin59@gmail.com>
2024-05-13 18:17:46 +0300

    complete

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    function listenFocus(word){
+        if(word === "focus"){
+            console.log("Good!")
+        }
+        else if(word === "blur"){
+            console.log("Hey! Eyes on me!")
+        }
+    }
+  return (
+    <div>
+        <button onClick={listenFocus}>Eyes on me</button>
+    </div>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
+import React from 'react'
 
 function Keypad (){
+    function printPwd(event){
+        console.log("Entering password...")
+        console.log(`${event.target.value}`)
+    }
     return (
-        <div></div>
+        <div>
+            <input type='password' onChange={printPwd} name='search'/>
+        </div>
     )
 }
 
